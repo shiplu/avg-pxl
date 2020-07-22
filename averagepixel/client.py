ba6eefaa1d7e67479977b52e1ee5c13c11313101
Shiplu Mokaddim <shiplu.net@gmail.com>
2020-07-22 07:34:19 +0200

    update client to make it re-usable

--- averagepixel/client.py
+++ averagepixel/client.py
@@ -3,6 +3,8 @@ calculate average pixel value of an image using our GRPC microservice
 """
 import logging
 import argparse
+from contextlib import contextmanager
+
 import grpc
 
 import imagestatistics_pb2
@@ -10,6 +12,11 @@ import imagestatistics_pb2_grpc
 
 
 def cli_args():
+    """Parses command line interface arguments
+
+    Returns:
+        argparser.Namespace: parsed arguments in a Namespace
+    """
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
@@ -18,11 +25,25 @@ def cli_args():
     parser.add_argument("--port", type=int, help="server port", default=30001)
     return parser.parse_args()
 
+@contextmanager
+def stub_client(host, port):
+    """Provides a GRPC stub to interact with server
+
+    Args:
+        host (string): GRPC server hostname
+        port (int): GRPC server port
+
+    Yields:
+        ImageStatisticsStub: local stub object
+    """
+    with grpc.insecure_channel("{}:{}".format(host, port)) as channel:
+        stub = imagestatistics_pb2_grpc.ImageStatisticsStub(channel)
+        yield stub
+
 
 def run():
     args = cli_args()
-    with grpc.insecure_channel("{}:{}".format(args.host, args.port)) as channel:
-        stub = imagestatistics_pb2_grpc.ImageStatisticsStub(channel)
+    with stub_client(args.host, args.port) as stub:
         response = stub.AgeragePixel(
             imagestatistics_pb2.ImageLocation(filepath=args.image)
         )
